d35fab2a6556a2a138a472aabed3df9767025225
sgujuluva <sanghee04@gmail.com>
2022-08-24 10:53:20 +0200

    navbar styled

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import "./Navbar.css"
 //images
 import logo from "../../images/logo1.png"
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 function Navbar() {
   return (
     <div classname="navbar">
@@ -10,14 +12,17 @@ function Navbar() {
        <div className="navbar-container">
         <div className="left">
        <img className='logo' src={logo} alt="" />
-        </div>
-      
-        <div className="right">
-            <span>Home</span>
+       <span>Home</span>
             <span>Series</span>
             <span>Movies</span>
             <span>New and Popular</span>
             <span>My List</span>
+        </div>
+      
+        <div className="right">
+        <SearchIcon/>
+        <span>Kids</span>
+        <NotificationsActiveIcon/>
         </div>
         </div> 
     </div>
