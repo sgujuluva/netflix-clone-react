daf86421df02347dbacc8de285f2b7f82c874104
sgujuluva <sanghee04@gmail.com>
2022-08-24 22:19:27 +0200

    usestate used to set the slide number

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,29 +1,55 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 //comp
 import ListItem from "../ListItem/index";
 import "./List.css";
 //icons
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 function List() {
-    let listRef = useRef();
-    const handleClick = (direction) => {
-        let distance=listRef.current.getBoundingClientRect().x -24;
-        if(direction === "left"){
-            listRef.current.style.transform = `translateX(${250+distance}px)`
-               }
+
+  const [slide, setSlide] = useState(0);
+
+  let listRef = useRef();
+
+  const handleClick = (direction) => {
+
+    let distance = listRef.current.getBoundingClientRect().x - 24;
+
+    console.log(distance)
+
+    if (direction === "left" && slide > 0) {
+      setSlide(slide - 1);
+      listRef.current.style.transform = `translateX(${250 + distance}px)`;
     }
+    if (direction === "right" && slide < 5) {
+      setSlide(slide + 1);
+      listRef.current.style.transform = `translateX(${-250 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <div className="wrapper">
-<ArrowBackIosIcon className="arrow-icon" onClick={() => handleClick("left")}/>
+        <ArrowBackIosIcon
+          className="arrow-icon left-ic"
+          onClick={() => handleClick("left")}
+        />
         <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
-        </div>
-    <ArrowForwardIosIcon className="arrow-icon" onClick={() => handleClick("right")}/>    
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+              </div>
+        <ArrowForwardIosIcon
+          className="arrow-icon right-ic"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
