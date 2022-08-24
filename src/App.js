7a9fe2d1d06a9ba72c9aea06274d08e83b0308a7
sgujuluva <sanghee04@gmail.com>
2022-08-24 08:59:01 +0200

    styling home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //styles
 import './App.css';
+//components
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-      Netflix
+     <Home/>
     </div>
   );
 }
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//styles
+import "./Home.css"
+function Home() {
+  return (
+    <div className="home">
+        Home
+        </div>
+  )
+}
+
+export default Home
