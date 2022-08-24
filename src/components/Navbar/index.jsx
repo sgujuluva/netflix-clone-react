734f728acda9659d3bc6a6af83988722ab81d42c
sgujuluva <sanghee04@gmail.com>
2022-08-24 13:42:47 +0200

    navbar styled

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React from "react";
 //styles
-import "./Navbar.css"
+import "./Navbar.css";
 //images
-import logo from "../../images/logo1.png"
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import logo from "../../images/logo4.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 function Navbar() {
   return (
-    <div classname="navbar">
-       
-       <div className="navbar-container">
+    <div className="navbar">
+      <div className="navbar-container">
         <div className="left">
-       <img className='logo' src={logo} alt="" />
-       <span>Home</span>
-            <span>Series</span>
-            <span>Movies</span>
-            <span>New and Popular</span>
-            <span>My List</span>
+          <img className="logo" src={logo} alt="" />
+          <span>Home</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
         </div>
-      
+
         <div className="right">
-        <SearchIcon/>
-        <span>Kids</span>
-        <NotificationsActiveIcon/>
+          <SearchIcon />
+          <span>Kids</span>
+          <NotificationsActiveIcon />
+          <img
+            className="profile-pic"
+            src="https://cdn.pixabay.com/photo/2015/10/09/00/55/lotus-978659__340.jpg"
+            alt=""
+          />
+          <ArrowDropDownIcon />
         </div>
-        </div> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
