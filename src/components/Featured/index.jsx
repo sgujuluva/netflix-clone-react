36ad6877cdcf059a2119e02db53e6dbe017c4a62
sgujuluva <sanghee04@gmail.com>
2022-08-24 15:34:38 +0200

    featured component buttons image creation

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import "./Featured.css"
+import React from "react";
+import "./Featured.css";
+//icons
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoIcon from "@mui/icons-material/Info";
 
 function Featured() {
   return (
-    <div>Featured</div>
-  )
+    <div className="featured">
+      <img
+        src="https://media.istockphoto.com/photos/father-and-daughter-day-picture-id584570368?b=1&k=20&m=584570368&s=170667a&w=0&h=bpjAb4pVSIYO63hXJR94UCX83d_fFbd4-PgogYWsu34="
+        alt=""
+      />
+      <span className="description">
+        If you want a happy ending, that depends, of course, on where you stop
+        your story. ... Always make the audience suffer as much as possible. ...
+        Cinema is the most beautiful fraud in the world. ... The sound and music
+        are 50% of the entertainment in a movie. ... Cinema is a matter of
+        what's in the frame and what's out.
+      </span>
+      <div className="button">
+        <button>
+          <PlayArrowIcon />
+         <span>Play</span> 
+        </button>
+        <button>
+          <InfoIcon />
+          <span>More Info</span> 
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Featured
+export default Featured;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Featured from "../Featured";
 import Navbar from "../Navbar/index";
 //styles
 import "./Home.css";
@@ -6,6 +7,7 @@ function Home() {
   return (
     <div className="home">
       <Navbar />
+      <Featured/>
     </div>
   );
 }
