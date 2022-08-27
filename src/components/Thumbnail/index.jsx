c0789b93614872a8e77ed5ab149a6e8577a5495d
sgujuluva <sanghee04@gmail.com>
2022-08-27 22:23:40 +0200

    thumbnail styled for trending now

--- src/components/Thumbnail/index.jsx
+++ src/components/Thumbnail/index.jsx
@@ -3,8 +3,10 @@ import "./Thumbnail.css"
 
 function Thumbnail({movieThumbnail}) {
   return (
+    <div className="thumbnail-main-container">
     <div className="thumbnail-container">
        <img src={`https://image.tmdb.org/t/p/w500${movieThumbnail.backdrop_path || movieThumbnail.poster_path }`} alt="" />
+    </div>
     </div>
   )
 }
