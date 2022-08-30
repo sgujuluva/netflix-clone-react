2c0ca2ad67bd97d4ade7b1798e69706a03266129
sgujuluva <sanghee04@gmail.com>
2022-08-30 15:03:41 +0200

    isscroll worked

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,55 +9,50 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if(window.scrollY > 0){
-        setIsScrolled(true)
-      }else{
-        setIsScrolled(false)
-      }
+  const handleScroll = () => {   
+    if (window.scrollY >= 30) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
     }
-    window.addEventListener("scroll",handleScroll)
-    return () => {
-      window.removeEventListener("scroll",handleScroll)
-    }
-  },[]);
-
+  };
+  window.addEventListener("scroll", handleScroll);
+ 
   return (
-   <div className={`${isScrolled && "backgroundColor:red"}`}>
-
-    <div className="navbar">
-      <div className="navbar-container">
-        <div className="left">
-          <img className="logo" src={logo} alt="" />
-          <span>Home</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New & Popular</span>
-          <span>My List</span>
-        </div>
-
-        <div className="right">
-          <SearchIcon />
-          <span>Kids</span>
-          <NotificationsActiveIcon />
-          <img
-            className="profile-pic"
-            src="https://cdn.pixabay.com/photo/2015/10/09/00/55/lotus-978659__340.jpg"
-            alt=""
-          />
-          <div className="profile">
-          <ArrowDropDownIcon />
-          <div className="options">
-          <span>Settings</span>
-          <span>Logout</span>
+    <div>
+      <div
+        className="navbar"
+        style={{ backgroundColor: isScrolled && "black" }}
+      >
+        <div className="navbar-container">
+          <div className="left">
+            <img className="logo" src={logo} alt="" />
+            <span>Home</span>
+            <span>Series</span>
+            <span>Movies</span>
+            <span>New & Popular</span>
+            <span>My List</span>
           </div>
-          
+
+          <div className="right">
+            <SearchIcon />
+            <span>Kids</span>
+            <NotificationsActiveIcon />
+            <img
+              className="profile-pic"
+              src="https://cdn.pixabay.com/photo/2015/10/09/00/55/lotus-978659__340.jpg"
+              alt=""
+            />
+            <div className="profile">
+              <ArrowDropDownIcon />
+              <div className="options">
+                <span>Settings</span>
+                <span>Logout</span>
+              </div>
+            </div>
           </div>
-          
         </div>
       </div>
-    </div>
     </div>
   );
 }
