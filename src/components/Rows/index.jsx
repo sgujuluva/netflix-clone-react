626f36b63a7fbdb6089ec661917dff22e7da10cd
sgujuluva <sanghee04@gmail.com>
2022-08-30 15:54:41 +0200

    trying bootstrap carousel

--- src/components/Rows/index.jsx
+++ src/components/Rows/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import {motion} from "framer-motion"
 //components
 import Thumbnail from "../Thumbnail";
+import Carousel from 'react-bootstrap/Carousel';
 
 //icons
 import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
@@ -18,22 +19,65 @@ function Rows({ title, movie }) {
   },[])
 
   return (
-    <div className="main-row">
+   /*  <div className="main-row">
     <motion.div className="main-list">
+     
       <h2>{title}</h2>
+    
+
+     
+    
       <motion.div drag="x" className="movie-list" ref={carousel}>
        
-         <ChevronLeftOutlinedIcon />
+        
         <motion.div drag="x" dragConstraints={{right:0}} className="thumbnail">
      {movie.map((item) => (
       <Thumbnail itemThumbnail={item}/>
      ))}
         </motion.div>
-       <ChevronRightOutlinedIcon /> 
+      
         
       </motion.div>
     </motion.div>
-    </div>
+    </div> */
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=373940"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>{title}</h3>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Second slide&bg=282c34"
+        alt="Second slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   );
 }
 
